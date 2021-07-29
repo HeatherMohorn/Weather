1f91bec90a6ffbc836b8fe8456fb398c50418387
Heather Mohorn <heather.mohorn@gmail.com>
2021-07-29 12:52:42 -0400

    Initial setup and starter code complete

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 /* Middleware */
 //Configuring express to use body-bodyParser
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
