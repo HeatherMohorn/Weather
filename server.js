ee8375a006779f42764c9d1a392e641d30450d80
Heather Mohorn <heather.mohorn@gmail.com>
2021-07-30 13:32:18 -0400

    Successfully returns user data and temp info

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [];
 
 //Require Express to run server and routes
 const express = require('express');
@@ -28,11 +28,31 @@ function listening(){
   console.log("server running on localhost: " + port);
 };
 
-app.get('/all', getData);
+
+//Step 2A
+app.get('/getData', getData);
 
 function getData(request, response) {
   response.send(projectData);
 }
+
+//Add POST route to add new data to projectData
+//Set up post route on server side and executed on client side as async function
+app.post('/addData', addData);
+
+function addData(request, response){
+  let newData = request.body;
+  let newEntry = {
+    temp: newData.temp,
+    feeling: newData.feeling,
+    date: newData.date
+  }
+  projectData.push(newEntry);
+  console.log(projectData);
+}
+
+
+
 
 //app.post('/add', postFunc);
 
@@ -40,3 +60,42 @@ function getData(request, response) {
 //  projectData.add(request.body);
 //  console.log(projectData);
 //}
+
+//Step 2B ??????
+/*
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+    const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+    try {
+      const newData = await response.json();
+      console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    }
+}*/
+
+//Step 2B trying again
+/*
+app.get('/add', postData);
+
+function postData(request, response){
+  let data = request.body;
+  projectData['temp'] = data.temp;
+  projectData['feeling'] = data.feeling;
+  projectData['date'] = data.date;
+  response.send(projectData);
+
+}
+
+postData('/add', {temp: 80, date: "7/30/21", feeling: "happy"});
+
+*/
--- website/app.js
+++ website/app.js
@@ -1,58 +1,69 @@
 /* Global Variables */
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-let key = "9f2d67b95c319a699b2f533755b62b02";
-
-//api call = base + zipcode + ',' '&appid='+key
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const key = "9f2d67b95c319a699b2f533755b62b02";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-/*
-const postData = async ( url = '', data = {})=>{
-    console.log(data);
-    const response = await fetch(url, {
-    method: 'POST',
-    credentials: 'same-origin',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
 
-    try {
-      const newData = await response.json();
-      console.log(newData);
-      return newData
-    }catch(error) {
-    console.log("error", error);
-    }
-}
-
-
-postData('/add', {temperature: 80, date: "7/30/21", userResponse: "happy"});
-*/
-
-//Step 3
-
+//add event listener for button
 document.getElementById('generate').addEventListener('click', performAction);
 
+//do this when clicked
 function performAction(e){
   const zip =  document.getElementById('zip').value;
-  getWeather(baseURL,zip,key);
-}
+  const feeling = document.getElementById('feelings').value;
+  getWeather(baseURL,zip,key)
+  .then(function(data){
+    console.log(data);
+    postData('/addData', {temp: data.main.temp, feeling: feeling, date: '7/30/21'});
+  })
 
+};
+
+//get temperature data from API
 const getWeather = async (baseURL, zip, key)=>{
-
   const response = await fetch(baseURL+zip+'&appid='+key+'&units=imperial')
   try {
-
     const data = await response.json();
     //console.log(response.main.temp);
     console.log(data.main.temp);
     return data;
   }  catch(error) {
     console.log("error", error);
-    // appropriately handle the error
   }
 }
+
+//async function to fetch data from app endpoint
+
+const getData = async (url = '') =>{
+  const request = await fetch(url);
+  try{
+    const theData = await request.json();
+  }
+  catch(error){
+    console.log("error", error);
+  }
+}
+
+//async post data function
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
